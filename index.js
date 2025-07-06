fcd5fe4d7ca091b04c7bf4450344d5d0406c8502
NAMBIRAJA-M <nambirajam.ug.21.cs@scadengineering.ac.in>
2025-07-06 22:53:36 +0530

    implemented .env file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
-
+dotenv.config(); 
 const db = new pg.Client({
-  user: "postgres",
+  /* user: "postgres",
   host: "localhost",
   database: "world",
   password: "2021",
-  port: 5432,
+  port: 5432, */
+   user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 db.connect();
 
